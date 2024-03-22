d36ee9acb47cac6452eeeb204878ec9e42a8c4f6
Filippo <pippo.faccini@gmail.com>
2024-03-22 17:41:05 +0100

    feat: basic plugin implementation

--- plugin/plugin.cpp
+++ plugin/plugin.cpp
@@ -1,1 +1,14 @@
 #include "plugin.hpp"
+
+void *pluginInitialize() {
+  printf("Hello from plugin\n");
+  printf("pluginInitialize\n");
+  return nullptr;
+}
+void pluginRun(void *pluginData [[maybe_unused]],
+               message_signals &resampledData [[maybe_unused]]) {
+  printf("pluginRun\n");
+}
+void pluginDeinitialize(void *pluginData [[maybe_unused]]) {
+  printf("pluginDeinitialize\n");
+}
--- plugin/plugin.hpp
+++ plugin/plugin.hpp
@@ -1,1 +1,7 @@
 #pragma once
+
+#include "buffer.hpp"
+
+void *pluginInitialize();
+void pluginRun(void *pluginData, message_signals &resampledData);
+void pluginDeinitialize(void *pluginData);
