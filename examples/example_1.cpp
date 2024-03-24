fbb3b36744b0b07f4b92ee4f4d1fb3351f43e3e5
Filippo <pippo.faccini@gmail.com>
2024-03-24 17:56:03 +0100

    feat: add example 1

---
+++ examples/example_1.cpp
@@ -0,0 +1,13 @@
+#include "plugin.hpp"
+
+extern "C" void *pluginInitialize() { return nullptr; }
+extern "C" void pluginDeinitialize(void *pluginData [[maybe_unused]]) {}
+
+extern "C" void pluginRun(void *pluginData [[maybe_unused]],
+                          message_signals &resampledData [[maybe_unused]]) {
+  resampledData["power"] = resampledData["INV_L_RCV.iq_actual"] *
+                               resampledData["INV_L_RCV.n_actual_filt"] +
+                           resampledData["INV_R_RCV.iq_actual"] *
+                               resampledData["INV_R_RCV.n_actual_filt"];
+  resampledData["power_kW"] = resampledData["power"] / 1000.0;
+}
--- plugin/plugin.cpp
+++ plugin/plugin.cpp
@@ -4,14 +4,11 @@ extern "C" void *pluginInitialize() {
   printf("Hello from plugin\n");
   return nullptr;
 }
-extern "C" void pluginRun(void *pluginData [[maybe_unused]],
-                          message_signals &resampledData [[maybe_unused]]) {
-  resampledData["power"] = resampledData["INV_L_RCV.iq_actual"] *
-                               resampledData["INV_L_RCV.n_actual_filt"] +
-                           resampledData["INV_R_RCV.iq_actual"] *
-                               resampledData["INV_R_RCV.n_actual_filt"];
-  resampledData["power_kW"] = resampledData["power"] / 1000.0;
-}
 extern "C" void pluginDeinitialize(void *pluginData [[maybe_unused]]) {
   printf("Bye from plugin\n");
+}
+
+extern "C" void pluginRun(void *pluginData [[maybe_unused]],
+                          message_signals &resampledData [[maybe_unused]]) {
+  // Process data here
 }
